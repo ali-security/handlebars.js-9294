617dd3eb3df49bafdc1dd8ca4298c70fb7319569
Alon Ohana <alon.o@sealsecurity.io>
2026-06-03 16:15:29 +0300

    test safari ehh

--- tests/browser/playwright.config.js
+++ tests/browser/playwright.config.js
@@ -10,10 +10,6 @@ const config = {
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] }
-    },
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] }
     }
   ],
   reporter: 'list',
