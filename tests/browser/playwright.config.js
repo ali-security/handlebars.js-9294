54c93c719bda806888773edf5343b1f0b99b7951
Alon Ohana <alon.o@sealsecurity.io>
2026-06-03 16:16:10 +0300

    unx

--- tests/browser/playwright.config.js
+++ tests/browser/playwright.config.js
@@ -10,6 +10,10 @@ const config = {
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] }
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] }
     }
   ],
   reporter: 'list',
